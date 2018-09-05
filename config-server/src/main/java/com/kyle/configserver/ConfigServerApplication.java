c45f3a78901fc846b35f45977c8efec4bb6f634b
kyle <624701089@qq.com>
2018-09-05 21:35:32 +0800

    Initial commit

---
+++ config-server/src/main/java/com/kyle/configserver/ConfigServerApplication.java
@@ -0,0 +1,21 @@
+package com.kyle.configserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+* @ author: Administrator
+* @ Description: EnableConfigServer 注解开启配置服务器功能
+* @ params:        
+* @ date: 21:25 2018/9/5
+* @ return: 
+**/        
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerApplication.class, args);
+    }
+}
