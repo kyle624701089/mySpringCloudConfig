891482807f70074f909bf2eb8258fc91b0ae4e14
kyle <624701089@qq.com>
2018-09-06 21:02:15 +0800

    增加config客户端

--- config-server/src/main/java/com/kyle/configserver/ConfigServerApplication.java
+++ config-server/src/main/java/com/kyle/configserver/ConfigServerApplication.java
@@ -7,8 +7,8 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 /**
 * @ author: Administrator
 * @ Description: EnableConfigServer 注解开启配置服务器功能
-* @ params:        
-* @ date: 21:25 2018/9/5
+* @ params:
+* @ date: 21:57 2018/9/5
 * @ return: 
 **/        
 @SpringBootApplication
